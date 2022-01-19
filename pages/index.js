f47f94c7d4dc20b2a696e5d7d62e5d818772a279
mattwhitson <matthew_2535@yahoo.ca>
2022-01-19 14:07:20 -0800

    one more minor change :)

--- pages/index.js
+++ pages/index.js
@@ -81,6 +81,7 @@ export default function Home({ initalPictures }) {
           content="Photos from NASA's Astronomy Photo of the Day (APOD) API"
         />
         <meta property="og:image" content={data[0]?.url} />
+        <meta name="theme-color" content="#0e0e0e" />
       </Head>
       <section className="max-w-xl mx-auto space-y-2 pt-4 px-1 sm:px-0">
         <FilterMenu
