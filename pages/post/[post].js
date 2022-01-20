d1a0d10a3b00d7d96d0652585dbda4b108116f5e
mattwhitson <matthew_2535@yahoo.ca>
2022-01-19 20:41:03 -0800

    some small final touches

--- pages/post/[post].js
+++ pages/post/[post].js
@@ -34,20 +34,20 @@ const Post = ({ post }) => {
           <meta property="og:video" content={post.url} />
         )}
       </Head>
-      <div className="grid grid-cols-1 xl:grid-cols-4 xl:max-w-[1500px] mx-auto sm:pt-4 shadow-lg dark:shadow-2xl ">
-        <section className="col-span-1 xl:col-span-3">
+      <div className="grid grid-cols-1 xl:grid-cols-4 xl:max-w-[1500px] mx-auto sm:pt-4 dark:shadow-2xl ">
+        <section className="col-span-1 xl:col-span-3 shadow-2xl">
           {post.media_type === "image" ? (
-            <img src={post.hdurl} alt="" className="rounded-l-md" />
+            <img src={post.hdurl} alt="" className="xl:rounded-l-md" />
           ) : (
             <div className="relative h-full pb-[50%] xl:pb-[1%]">
               <iframe
-                className="absolute top-0 left-0 w-full h-full rounded-l-md"
+                className="absolute top-0 left-0 w-full h-full xl:rounded-l-md"
                 src={post.url}
               ></iframe>
             </div>
           )}
         </section>
-        <section className="col-span-1 p-4 rounded-r-md bg-[#f7f7f7] dark:bg-[#161616] sm:dark:bg-[#1a1a1a] space-y-2 shadow-lg">
+        <section className="col-span-1 p-4 rounded-r-md bg-[#f7f7f7] dark:bg-[#161616] sm:dark:bg-[#1a1a1a] space-y-2 shadow-2xl dark:shadow-lg">
           <h2 className="text-2xl sm:text-4xl font-semibold">{post.title}</h2>
           <time className="text-sm font-semibold">{post.date}</time>
           {post.copyright && (
